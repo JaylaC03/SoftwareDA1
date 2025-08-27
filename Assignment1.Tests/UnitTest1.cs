872b709ef92c5c9b7b79430de795baecddd1bf60
jayla <jayla@MOGUL444>
2025-08-27 16:30:34 -0400

    Add project files.

---
+++ Assignment1.Tests/UnitTest1.cs
@@ -0,0 +1,87 @@
+﻿using Assignment1.library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Assignment1.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void OnlyLowercase_ShouldBeWeak()
+        {
+            PasswordChecker checker = new PasswordChecker();
+            string result = checker.CheckPassword("abc");
+            Assert.AreEqual("WEAK", result);
+        }
+
+        [TestMethod]
+        public void OnlyUppercase_ShouldBeWeak()
+        {
+            PasswordChecker checker = new PasswordChecker();
+            string result = checker.CheckPassword("ABC");
+            Assert.AreEqual("WEAK", result);
+        }
+
+        [TestMethod]
+        public void OnlyDigits_ShouldBeWeak()
+        {
+            PasswordChecker checker = new PasswordChecker();
+            string result = checker.CheckPassword("123");
+            Assert.AreEqual("WEAK", result);
+        }
+
+        [TestMethod]
+        public void OnlySpecialChars_ShouldBeWeak()
+        {
+            PasswordChecker checker = new PasswordChecker();
+            string result = checker.CheckPassword("@#$");
+            Assert.AreEqual("WEAK", result);
+        }
+
+        [TestMethod]
+        public void LowercaseAndUppercase_ShouldBeMedium()
+        {
+            PasswordChecker checker = new PasswordChecker();
+            string result = checker.CheckPassword("abcDEF");
+            Assert.AreEqual("MEDIUM", result);
+        }
+
+        [TestMethod]
+       public void LowerUpperAndDigits_ShouldBeMedium()
+        {
+            PasswordChecker checker = new PasswordChecker();
+            string result = checker.CheckPassword("abcDEF123");
+            Assert.AreEqual("MEDIUM", result);
+        }
+        [TestMethod]
+        public void LowerUpperAndSpecial_ShouldBeMedium()
+        {
+            PasswordChecker checker = new PasswordChecker();
+            string result = checker.CheckPassword("abcDEF@#");
+            Assert.AreEqual("MEDIUM", result);
+        }
+        [TestMethod]
+        public void AllCriteriaMet_ShouldBeStrong()
+        {
+            PasswordChecker checker = new PasswordChecker();
+            string result = checker.CheckPassword("abcDEF123@#");
+            Assert.AreEqual("STRONG", result);
+        }
+        [TestMethod]
+        public void EmptyPassword_ShouldBeIneligible()
+        {
+            PasswordChecker checker = new PasswordChecker();
+            string result = checker.CheckPassword("");
+            Assert.AreEqual("INELIGIBLE", result);
+        }
+
+        [TestMethod]
+        public void SingleCharacter_ShouldBeWeak()
+        {
+            PasswordChecker checker = new PasswordChecker();
+            string result = checker.CheckPassword("a");
+            Assert.AreEqual("WEAK", result);
+        }
+    }
+}
---
+++ Assignment1.library/PasswordChecker.cs
@@ -0,0 +1,37 @@
+﻿// imports" that give us extra tools to work with
+using System; //Basic tools for C# (like Console, strings, etc.)
+using System.Linq; //Gives us ".Any()" which helps us check conditions inside strings
+
+namespace Assignment1.library
+{
+    public class PasswordChecker
+    {
+        //method that checks how strong a password is
+        public string CheckPassword(string password)
+        {
+            bool hasUpper = password.Any(char.IsUpper); //checks if there's at least one uppercase letter
+            bool hasLower = password.Any(char.IsLower); //checks if there's at least one lowercase letter
+            bool hasDigit = password.Any(char.IsDigit); //checks if there's at least one digit
+            bool hasSpecial = password.Any(ch => !char.IsLetterOrDigit(ch)); //checks if there's at least one special character
+
+            int criteriaMet = 0; //counter for how many criteria are met
+
+            if (hasUpper) criteriaMet++;
+            if (hasLower) criteriaMet++;
+            if (hasDigit) criteriaMet++;
+            if (hasSpecial) criteriaMet++;
+
+            // Determine strength based on criteria met and length
+
+            return criteriaMet switch
+            {
+                0 => "INELIGIBLE",
+                1 => "WEAK",
+                2 or 3 => "MEDIUM",
+                4 => "STRONG",
+                _ => "INELIGIBLE" 
+            };
+
+        }
+    }
+}
